7044f9cac2f31a610a138d241d8d0a3cce5f557d
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-07 22:22:36 +0400

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def lowercaseAlphabets():
-    # lowercase
-   for i in 'abcdefghijklmnopqrstuvwxyz':
+for i in 'abcdefghijklmnopqrstuvwxyz':
     if (i != "q" and i != "e"):
         print("{}".format(i), end="")
-lowercaseAlphabets()
