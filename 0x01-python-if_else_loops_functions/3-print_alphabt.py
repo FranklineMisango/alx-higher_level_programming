ccd7e15789c13a9abde9e3ac642e9e444371e62b
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-07 20:05:46 +0400

    Add files via upload

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def lowercaseAlphabets():
+    # lowercase
+    for c in range(97, 123):
+        if c == 101:
+            pass
+        elif c == 113:
+            pass
+        else:
+            print(chr(c), end=" ")
+lowercaseAlphabets()
