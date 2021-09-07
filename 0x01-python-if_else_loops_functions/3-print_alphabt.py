a7edc6b034ce2f79a80c31fd11b90d8e1949a1eb
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-07 22:02:51 +0400

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def lowercaseAlphabets():
     # lowercase
-    for c in range(97, 123):
-        if c == 101:
-            pass
-        elif c == 113:
-            pass
-        else:
-            print(chr(c), end=" ")
+   for i in 'abcdefghijklmnopqrstuvwxyz':
+    if (i != "q" and i != "e"):
+        print("{}".format(i), end="")
 lowercaseAlphabets()
