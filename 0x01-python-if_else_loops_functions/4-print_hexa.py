7c4b4369aafa0026281527ee29db5e9244406b1e
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-07 22:59:07 +0400

    Update 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def hexadecimal_numbers():
-    for i in range(0, 99):
-        print("{} = {}".format(i, hex(i)))
-hexadecimal_numbers()
+for i in range (99):
+    print("{} = {}".format(i, hex(i)))
