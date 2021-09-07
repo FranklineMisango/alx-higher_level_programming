7266d16d2ef12fc065ed1067053c3d4ef64f5706
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-07 22:41:10 +0400

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def lowercaseAlphabets():
-    for a in "abcdefghijklmnopqrstuvwxyz":
-        print('{}'.format(a), end="")
-lowercaseAlphabets()
+for a in "abcdefghijklmnopqrstuvwxyz":
+    print('{}'.format(a), end="")
