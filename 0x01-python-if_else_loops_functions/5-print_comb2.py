05dc449387406a79b8170b5064d2d890dd49bd15
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-07 22:05:59 +0400

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def comb2():
-    for i in range(0, 100):
-        print("{:02d}, ".format(i), end="")
-        if i == 99:
-            print("{:02d}".format(i))
-comb2()
+for i in range(100):
+    print("{:0>2}".format(i), end=", " if i < 99 else "\n")
+    
