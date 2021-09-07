490e8344c2ff9407cfeeeaff9959cf4e4be0f858
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-07 23:03:13 +0400

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print(
-            "{}{}".format(i, j),
-            end=", " if int(str(i) + str(j)) < 89 else "\n"
-            )
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i != j:
+            print("{}{}".format(i, j), end=" ")
+print("\n")
