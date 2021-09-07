141b87dbdb8e6c55c84615df5c1f42db1f65d773
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-07 22:37:51 +0400

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 15 == 0:
+        if (i % 15 == 0):
             print("FizzBuzz ", end="")
-        elif i % 3 == 0:
+        elif (i % 3 == 0):
             print("Fizz ", end="")
-        elif i % 5 == 0:
+        elif (i % 5 == 0):
             print("Buzz ", end="")
         else:
-            print("{} ".format(i), end="")
-fizzbuzz()
+            print("{:d} ".format(i), end="")
+ 
