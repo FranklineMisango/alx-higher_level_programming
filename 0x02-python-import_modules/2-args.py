06e6b01f6822e138bcc9ebb8d51e1b9240a82987
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-09 22:25:37 +0400

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     args = len(sys.argv)
     if args == 1:
-        print("{} arguments.".format(args - 1))
+        print("{:d} arguments.".format(args - 1))
     else:
-        print("{} arguments:".format(args - 1))
+        print("{:d} arguments:".format(args - 1))
     x = 1
     for arg in sys.argv[1:]:
-        print("{}: {}".format(x, arg))
+        print("{:d}: {}".format(x, arg))
         x += 1
