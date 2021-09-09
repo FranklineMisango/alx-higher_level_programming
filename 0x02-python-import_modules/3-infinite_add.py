798b916ba7de4452d0896c1992da223b30d66ceb
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-09 22:40:20 +0400

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-
-    if len(argv) == 1:
-        print('{:d}'.format(len(argv)-1))
-
-    elif len(argv) > 1:
-        sum = 0
-        for i in range(1, len(argv)):
-            sum += int(argv[i])
-        print('{:d}'.format(sum))
+    num_args = len(argv)
+    total = 0
+    for i in range(1, num_args):
+        total += int(argv[i])
+    print("{:d}".format(total))
