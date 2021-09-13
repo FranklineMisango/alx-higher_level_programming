281c0879473faf613eb579886260c2746a1dcb66
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-13 19:39:12 +0400

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if not my_list:
-        return
     if (0 <= idx < len(my_list)):
-            my_list[idx] = element
-            return my_list
+        my_list[idx] = element
+        return my_list
