9b187229271da075d672e93370ea82d1fc7563b9
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-14 19:14:37 +0400

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    import hidden_4
-    all_dir = dir(hidden_4)
-    for i in range(0, len(all_dir)):
-        if "__" != all_dir[i][:2]:
-            print(all_dir[i])
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
