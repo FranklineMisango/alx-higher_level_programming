cd5f67e2d199c8b6cad706527a7f002f4e03537c
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-14 18:41:19 +0400

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,22 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a singly linked list has a cycle in it
- *  @list: singly list to check
- * Return: 0 if there's no cycle, 1 if there is a cycle
+ * check_cycle - check if list is cycle
+ * @list: type listint_t
+ * Return: always int
+ * case: 1 true, 0 false
  */
 
 int check_cycle(listint_t *list)
 {
-    listint_t *current;
+	listint_t *turtle = list, *hare = list;
 
-    if (list == NULL)
-    return (0);
-
-    current = list->next;
-
-    while (current != NULL)
-    {
-        if (current == list)
-        return (1);
-        current = current->next;
-    }
-
-    return(0);
+	while (turtle != NULL && hare != NULL && hare->next != NULL)
+	{
+		turtle = turtle->next;
+		hare = hare->next->next;
+		if (turtle == hare)
+			return (1);
+	}
+	return (0);
 }
