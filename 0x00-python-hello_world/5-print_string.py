bc4d53306269a9b37bfc6957603d771ea96ddcef
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-14 22:37:57 +0400

    Update 5-print_string.py

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 str = "Holberton School"
-#print(str * 3)
-#print(str[:9])
-print("{}{}{}{}".format((str * 3), /n, (str[:9]), /n)
+print("{} \n {} \n".format((str * 3), (str[:9])))
