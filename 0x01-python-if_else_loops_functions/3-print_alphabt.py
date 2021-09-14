364fe6eeb84b3b9fa3176b6265d61d82c8582388
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-14 19:05:04 +0400

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
+  
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != 'e'and i != 'q':
-        print("{}".format(chr(i)), end="")
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) == 'e' or chr(i) == 'q':
+        continue
+    else:
+        print("{:s}".format(chr(i)), end="")
