f16d9bdad3231bd9c1ec3af3c6f7f91eb67ca4d4
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-27 17:28:09 +0400

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,10 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[index]), end='')
             cmpt += 1
-        except IndexError:
-            break
-        except Exception:
-            pass
+        except (ValueError, TypeError):
+            continue
 
     print('')
     return cmpt
