dd26cbfdb8d328a42c561c19e9614b65444de8ee
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-27 17:20:16 +0400

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,8 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
 
-except Exception as error:
+    except:
         return False
+        
     else:
         return True
