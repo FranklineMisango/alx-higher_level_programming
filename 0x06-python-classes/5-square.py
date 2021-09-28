2bacda0cea7d3632c89588ed8faafa7d453cb2f7
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-28 18:28:17 +0400

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class
+Its implements value and type checks for its attributes
+"""
+
+
 class Square:
+    """Square implementation
+    """
     def __init__(self, size=0):
         self.__size = size
 
@@ -10,18 +18,22 @@ class Square:
     @size.setter
     def size(self, size):
         if type(size) != int:
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         self.__size = size
 
     def area(self):
-        return (self.__size * self.__size)
+        """calculates the square area
+        """
+        return (self.__size ** 2)
 
     def my_print(self):
-        if self.__size == 0:
-            print(" ")
+        """prints a square  with the corresponding size
+        """
+        if (self.__size == 0):
+            print('')
 
-        else:
-            for x in range(self.__size):
-                print("#" * self.__size)
+        for l in range(self.__size):
+            print('#' * self.__size)
+            
