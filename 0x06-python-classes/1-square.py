15473e47bba7a896eadcd71616766994d4e00774
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-28 18:25:23 +0400

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
+"""
+This module defines a Square class
+"""
+
 class Square:
+    
+        """Square implementation
+    """
+        
     def __init__(self, size):
         self.__size = size
