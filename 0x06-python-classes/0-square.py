651be30fafb9de463dfc5593ee922eec395eb6ad
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-28 18:21:19 +0400

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+
+"""
+This module defines a Square class
+Its implements value and type checks for its attributes
+"""
+
 class Square:
+        """Square implementation
+    """
     pass
