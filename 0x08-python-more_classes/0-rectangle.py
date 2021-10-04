082e24a3c6074c2060a5cad5200e6320732c1f57
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-10-04 23:40:31 +0400

    Add files via upload

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+An empty class that defines a rectangle
+"""
+class Rectangle:
+    """A Rectangle class skeleton"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+Defines a class Rectangle with private attributes width & height
+Width and height must be integers otherwise raise a TypeError with a message
+They must be greater than zero else raise a ValueError with a message
+"""
+
+class Rectangle:
+    """
+    Rectangle object with getters & setters
+    """
+    def __init__(self, width=0, height=0):
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+
+            self.__height = value
