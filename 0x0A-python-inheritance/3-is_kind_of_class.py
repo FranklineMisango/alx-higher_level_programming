ad5d482a6b6f2cb389eca2759f85ea8cbeda2b8e
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-10-11 17:13:55 +0400

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,6 +6,7 @@ or if the object is an instance of a class that inherited from,/
 the specified class ; otherwise False
 """
 
+
 def is_kind_of_class(obj, a_class):
     """
     checks if an object is a child or a child of a child
