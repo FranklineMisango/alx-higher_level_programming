ff892e336a8b3ad1c6cac5d2b583633a4deccb3c
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-10-18 20:38:22 +0400

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,7 +30,7 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
-        
+
     @classmethod
     def save_to_file(cls, list_objs):
         """save to json file"""
