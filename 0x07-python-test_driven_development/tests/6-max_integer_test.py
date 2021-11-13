ccc8e7211930dff13292e91445d460dd6ba0a8a8
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-11-13 13:23:28 +0400

    "Done"

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
+"""Unittest for max_integer([..])
 """
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
+class TestMaxInteger(unittest.TestCase):
     """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+    Tests the max_integer file
+    """
+    def test_maxInteger(self):
+        """Checks if the right value is returned"""
+        self.assertAlmostEqual(max_integer([0, 1, 3, 5, 7]), 7)
+        self.assertAlmostEqual(max_integer([0, 9, 7]), 9)
+        self.assertAlmostEqual(max_integer([5, 0, 1, 3]), 5)
+        self.assertAlmostEqual(max_integer([0, 10, 5, 7]), 10)
+        self.assertAlmostEqual(max_integer([0, 10, -5, 7]), 10)
+        self.assertAlmostEqual(max_integer([-10, -5, -7]), -5)
+        self.assertAlmostEqual(max_integer([7]), 7)
+
+    def test_type(self):
+        """Checks that the right type is returned"""
+        self.assertRaises(ValueError, max_integer, ["a", 1, "s"])
+
+    def test_empty(self):
+        """Checks the result of an empty string"""
+        self.assertAlmostEqual(max_integer([]), None)
