c4df872d9d2c18cf692f72cb6c3ac05dd3b123e3
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-11-16 19:50:30 +0400

    Add files via upload

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+--Write a script that creates a table called first_table in the current database in your MySQL server
+--The database name will be passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
