1ba877120a5dbbec5341dedcbd1fc894046fbf77
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-11-17 16:49:09 +0400

    Update 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
---lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
 SELECT score, name 
 FROM second_table 
 HAVING name IS NOT NULL 
