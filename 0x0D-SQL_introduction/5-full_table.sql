dab70a7b7ae7e044949ce597cd14b2fb867f1849
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-11-17 17:25:58 +0400

    Update 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- prints the full description of the table first_table from the database hbtn_0c_0
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+SHOW CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
