cad461d37cae6481874ae16e00417efe22b79267
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-11-17 22:06:43 +0400

    Update 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,8 @@
 -- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
-SELECT s.title, g.name
-FROM tv_shows s
-LEFT JOIN tv_show_genres m ON s.id = m.show_id
-LEFT JOIN tv_genres g ON m.genre_id = g.id
-ORDER BY s.title ASC;
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id 
+ORDER BY tv_shows.title, tv_genres.name;
