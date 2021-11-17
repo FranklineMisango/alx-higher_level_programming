2fa6584fb3dc09a5ac5d8c4655c2b403195888e4
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-11-17 21:04:01 +0400

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE cities(
+CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL FOREIGN KEY,
     name VARCHAR(256) NOT NULL,
