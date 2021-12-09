d74462c342ff801442a372ed9fcd0eb6c4c60cf9
Benson King'ori <bensonkingori96@gmail.com>
2021-12-09 02:49:20 -0800

    h

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 10):
     for j in range(0, 10):
         if i != j:
             if i < j:
-                print("{}{},".format(i, j), end=" ")
                 if i == 8:
                     print("{}{}".format(i, j))
+                print("{}{},".format(i, j), end=" ")
