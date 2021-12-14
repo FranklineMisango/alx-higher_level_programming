d150f73fc1f3e5f0e56ec7dcef830b1ef44cf852
Virgo-Alpha <bensonkingori96@gmail.com>
2021-12-14 15:26:38 +0000

    d

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let newlist = list.reverse();
+  let newlist = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    newlist.push(list[i]);
+  }
   return newlist;
 };
