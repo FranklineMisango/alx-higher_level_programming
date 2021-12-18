32187a1d0985210f556799400775c60accc81a21
Virgo Alpha <bensonkingori96@gmail.com>
2021-12-18 18:11:21 +0000

    d

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     command = """SELECT cities.id, city.name, states.name
                  FROM cities
-                 INNER JOIN cities ON states.id = cities.state_id
+                 INNER JOIN states ON cities.state_id = states.id
                  ORDER BY cities.id ASC"""
 
     cur.execute(command)
