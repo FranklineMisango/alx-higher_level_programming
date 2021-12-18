af387e2b070073b8cd2baaf06730feeb94b7b1ee
Virgo Alpha <bensonkingori96@gmail.com>
2021-12-18 12:53:17 +0000

    d

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """Write a script that lists all states from the database hbtn_0e_0_usa:
-Your script should take 3 arguments: 
-mysql username, mysql password 
+Your script should take 3 arguments:
+mysql username, mysql password
 and database name (no argument validation needed)"""
 
 import sys
