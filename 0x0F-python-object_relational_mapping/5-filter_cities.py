e09ad5d1839a9793a22866e8e414b66aa869c09e
Virgo Alpha <bensonkingori96@gmail.com>
2021-12-19 10:35:59 +0000

    g

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""script that lists all states from the database hbtn_0e_0_usa:
+Your script should take 3 arguments:
+mysql username, mysql password
+and database name (no argument validation needed)"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    cur = db.cursor()
+    cur.execute('SELECT cities.id, cities.name, states.name\
+                FROM states\
+                INNER JOIN cities ON states.id = cities.state_id\
+                WHERE states.name=%s\
+                ORDER BY cities.id ASC;', (sys.argv[4],))
+    cities = cur.fetchall()
+
+    print(",".join([x[0] for x in cities]))
+    
+    cur.close()
+    db.close()
