f7c279ffda3493910194aad942b306564b4aaf71
Virgo Alpha <bensonkingori96@gmail.com>
2021-12-24 12:39:52 +0000

    e

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,11 +16,11 @@ if __name__ == "__main__":
                          port=3306)
 
     cur = db.cursor()
-    command = """SELECT id, name
+    command = "SELECT id, name
                FROM states
                WHERE name=%s
-               ORDER BY id ASC"""
-    cur.execute(command, (sys.argv[4],))
+               ORDER BY id ASC"
+    cur.execute(command, (sys.argv[4]))
     xStates = cur.fetchall()
 
     for state in xStates:
